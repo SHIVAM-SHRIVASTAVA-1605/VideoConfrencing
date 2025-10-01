d9c789f4a945fa562ba9a0e131012127162bf694
SHIVAM-SHRIVATSAVA-1605 <shivam091204@gmail.com>
2025-10-01 22:48:51 +0530

    Setting up Backend Structure

---
+++ backend/src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import {createServer} from "node:http";
+
+import {Server} from "socket.io";
+
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+
+app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({limit: "40kb", extended: true}));
+
+const start = async () => {
+
+    app.set("shivam_user")
+    const connectionDb = await mongoose.connect(process.env.MONGO_URL)
+    console.log(`Mongo connect DB host: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("Listen on port 8000")
+    });
+}
+
+start();
