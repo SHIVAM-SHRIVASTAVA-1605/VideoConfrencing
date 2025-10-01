b38cdaacca84a29e2832e89a5a4d947f9f1a4362
SHIVAM-SHRIVATSAVA-1605 <shivam091204@gmail.com>
2025-10-01 23:46:08 +0530

    Authentication Routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,8 @@ import { connectToSocket } from "./controllers/socketManager.js";
 
 import cors from "cors";
 
+import userRoutes from "./routes/usersRoutes.js";
+
 
 const app = express();
 const server = createServer(app);
@@ -20,6 +22,8 @@ app.set("port", (process.env.PORT || 8000))
 app.use(cors());
 app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb", extended: true}));
+
+app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
 
