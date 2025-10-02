83204dac758e52915352d9b5193b35938259d31f
SHIVAM-SHRIVATSAVA-1605 <shivam091204@gmail.com>
2025-10-02 14:20:15 +0530

    Creating Landing Page

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import "../App.css"
+import phone from '../assets/phone.png';
+import { Link } from "react-router-dom";
 
 export default function LandingPage() {
   return (
-    <div>LandingPage</div>
+    <div className='landingPageContainer'>
+        <nav>
+            <div className='navHeader'>
+                <h2 style={{ color: "white" }}>VideoConfrencing</h2>
+            </div>
+            <div className='navlist'>
+                <p>Join as Guest</p>
+                <p>Register</p>
+                <div role = 'button'>
+                    <p>Login</p>
+                </div>
+            </div>
+        </nav>
+
+        <div className='landingMainContainer'>
+            <div>
+                <h1 style={{ color: "white" }}><span style={{color: "#FF9839"}}>Connect</span> with you loved ones</h1>
+                <p style={{ color: "white" }}>Cover a distance by VideoConfrencing</p>
+                <div role='button'>
+                    <Link to="/home">Get Started</Link>
+                </div>
+            </div>
+            <div>
+                <img src={phone} alt="mobile screen" />
+            </div>
+        </div>
+
+    </div>
   )
 }
